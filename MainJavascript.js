eff331b5dad472bfe53af6500fb913256af89715
Jacob385 <122482836+Jacob385@users.noreply.github.com>
2025-09-09 17:58:45 -0500

    Simple starting point. Mainly just pings the API

---
+++ MainJavascript.js
@@ -0,0 +1,42 @@
+const ImageAPILink = "https://sfasu-cdn.presence.io/organization-photos/8317246b-60de-41af-9823-4a35504099d4/"
+const APILink = "https://api.presence.io/sfasu/v1/organizations"
+
+function update() {
+  
+    const clubData = JSON.parse(GetAPIJson(APILink));
+    clubData.forEach(createClub)
+
+}
+
+
+
+function createClub(club,index){
+    //create div
+    const clubElement= document.createElement("span");
+    clubElement.className = "club";
+
+    //add link to div
+    const link= document.createElement("a");
+    link.href = "https://sfasu.presence.io/organization/" + club.uri;
+    clubElement.appendChild(link);
+
+    //add pic to link
+    const clubPic = document.createElement("img");
+    clubPic.className = "crop";
+    clubPic.src = ImageAPILink + club.photoUri;
+    clubPic.alt = club.uri;
+    link.appendChild(clubPic);
+
+    //put div on page
+    document.body.appendChild(clubElement);   
+}
+   
+    
+function GetAPIJson(url){
+    var Httpreq = new XMLHttpRequest();
+    Httpreq.open("GET",url,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
+}
+
+          
